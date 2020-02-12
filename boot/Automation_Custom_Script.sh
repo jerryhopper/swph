6f18b7979364970d4bd552201877b41cc2c4872a
JerryHopper <hopper.jerry@gmail.com>
2020-02-12 17:51:03 +0100

    Update Automation_Custom_Script.sh

--- boot/Automation_Custom_Script.sh
+++ boot/Automation_Custom_Script.sh
@@ -14,10 +14,9 @@ echo "automation_custom_script.sh has started">>/boot/log.txt
 telegram()
 {
    local VARIABLE=${1}
-   apiToken=447794744:AAGrNj3vyDgH5BU_dxQqfDQjgIgeN250Q04
-   chatId=-1001402917482
-   curl -s -X POST https://api.telegram.org/bot$apiToken/sendMessage -d text="Automation_Custom_Script.sh : $VARIABLE" -d chat_id=$chatId >/dev/null
+   curl -s -X POST https://blackbox.surfwijzer.nl/telegram.php -d text="create_image : $VARIABLE" >/dev/null
 }
+
 telegram() "Check"
 
 
