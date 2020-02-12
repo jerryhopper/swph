fa4f0e46e24e92d41de9a8f8c0a3bed391fb5b61
jerryhopper <hopper.jerry@gmail.com>
2020-02-12 10:08:30 +0100

    initial

---
+++ boot/Automation_Custom_PreScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
+
+# Custom Script (pre-networking and pre-DietPi install)
+# - Allows you to automatically execute a custom script before network is up on first boot.
+# - Copy your script to /boot/Automation_Custom_PreScript.sh and it will be executed automatically.
+# - Executed script log: /var/tmp/dietpi/logs/dietpi-automation_custom_prescript.log
+
+FILE=/boot/blackbox/1_hardwaredetect.sh
+if [ -f "$FILE" ]; then
+    # the file exists!
+    # run hardware detection and create hardware.json & hardware.hash
+    echo "$FILE exist"
+    bash /boot/blackbox/1_hardwaredetect.sh
+    # after the hardwaretest, remove the script.
+    rm -f /boot/blackbox/1_hardwaredetect.sh
+fi
+
+echo "automation_custom_prescript has run">>/boot/log.txt
---
+++ boot/Automation_Custom_Script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
+echo "automation_custom_script.sh has started">>/boot/log.txt
+# Custom Script (post-networking and post-DietPi install)
+# - Allows you to automatically execute a custom script at the end of DietPi install.
+# - Option 0 = Copy your script to /boot/Automation_Custom_Script.sh and it will be executed automatically.
+# - Option 1 = Host your script online, then use e.g. AUTO_SETUP_CUSTOM_SCRIPT_EXEC=https://myweb.com/myscript.sh and it will be downloaded and executed automatically.
+# - Executed script log: /var/tmp/dietpi/logs/dietpi-automation_custom_script.log
+
+#echo  $HARDWAREHASH>/var/log/sinit.log
+#echo  $POSTDATA>/boot/blackbox/hardware.json
+#echo  $HARDWAREHASH>/boot/blackbox/hardware.hash
+
+
+FILE=/boot/blackbox/hardware.json
+if [ -f "$FILE" ]; then
+    # the file exists!
+    # this means the hardware-detect has already run.
+    # we need to register the hardware in our product db
+    bash /boot/blackbox/2_registerhardware.sh
+fi
