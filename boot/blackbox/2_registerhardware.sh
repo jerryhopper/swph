7e007fa3be7aab671e99fe71e947dd6009fea246
jerryhopper <hopper.jerry@gmail.com>
2020-02-14 01:30:40 +0100

    Update 2_registerhardware.sh

--- boot/blackbox/2_registerhardware.sh
+++ boot/blackbox/2_registerhardware.sh
@@ -26,7 +26,7 @@ sendhash()
   -H "Accept: application/json" \
   -H "Content-Type:application/json" \
   -H "Authorization: $HARDWAREHASH" \
-  -X POST --data "$POSTDATA" "https://blackbox.surfwijzer.nl/api/installation?hash=$HARDWAREHASH")
+  -X POST --data "$POSTDATA" "https://blackbox.surfwijzer.nl/api/installation?ip=$IPV4_ADDRESS&hash=$HARDWAREHASH")
 
   # check if the post succeeds
   if [[ "$status_code" -ne 200 ]] ; then
@@ -43,12 +43,40 @@ sendhash()
     rm -f /boot/blackbox/hardware.json
     rm -f /boot/blackbox/hardware.hash
   fi
-
-
-
 }
 
+find_IPv4_information() {
+    # Detects IPv4 address used for communication to WAN addresses.
+    # Accepts no arguments, returns no values.
+
+    # Named, local variables
+    local route
+    local IPv4bare
+
+    # Find IP used to route to outside world by checking the the route to Google's public DNS server
+    route=$(ip route get 8.8.8.8)
+
+    # Get just the interface IPv4 address
+    # shellcheck disable=SC2059,SC2086
+    # disabled as we intentionally want to split on whitespace and have printf populate
+    # the variable with just the first field.
+    printf -v IPv4bare "$(printf ${route#*src })"
+    # Get the default gateway IPv4 address (the way to reach the Internet)
+    # shellcheck disable=SC2059,SC2086
+    printf -v IPv4gw "$(printf ${route#*via })"
+
+    if ! valid_ip "${IPv4bare}" ; then
+        IPv4bare="127.0.0.1"
+    fi
+
+    # Append the CIDR notation to the IP address, if valid_ip fails this should return 127.0.0.1/8
+    IPV4_ADDRESS=$(ip -oneline -family inet address show | grep "${IPv4bare}/" |  awk '{print $4}' | awk 'END {print}')
+}
+
+
+
 start(){
+  find_IPv4_information
   sendhash
 }
 
