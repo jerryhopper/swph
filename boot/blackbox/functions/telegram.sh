5b8fd1522441171fbee2ec49518bf3dd56dcf5ce
jerryhopper <hopper.jerry@gmail.com>
2020-02-20 15:54:02 +0100

    Update telegram.sh

--- boot/blackbox/functions/telegram.sh
+++ boot/blackbox/functions/telegram.sh
@@ -2,7 +2,7 @@
 telegram()
 {
    local VARIABLE=${1}
-   curl -s -X POST https://blackbox.surfwijzer.nl/telegram.php \
+   curl -s -X POST https://blackbox.surfwijzer.nl/api/telegram \
         -H "User-Agent: surfwijzerblackbox" \
         -H "Cache-Control: private, max-age=0, no-cache" \
         -H "X-Script: $SCRIPT_FILENAME" \
