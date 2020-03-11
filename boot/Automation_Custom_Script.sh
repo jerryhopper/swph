530f0fbd9b02333401a90315e6819142b5f7507b
jerryhopper <hopper.jerry@gmail.com>
2020-03-11 20:04:23 +0100

    Update Automation_Custom_Script.sh

--- boot/Automation_Custom_Script.sh
+++ boot/Automation_Custom_Script.sh
@@ -2,123 +2,55 @@
 # -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
 # We do not want users to end up with a partially working install, so we exit the script
 # instead of continuing the installation with something broken
-set -e
+#set -e
+#SCRIPT_FILENAME="Automation_Custom_Script"
+#echo "Automation_Custom_Script.sh has started">>/boot/log.txt
 
-SCRIPT_FILENAME="Automation_Custom_Script"
+curl -sSL https://raw.githubusercontent.com/jerryhopper/osbox/master/advanced/installation.sh | bash
 
-echo "Automation_Custom_Script.sh has started">>/boot/log.txt
-
-
-if [ -f "/etc/blackbox/blackbox.conf" ]; then
-  source "/etc/blackbox/blackbox.conf"
-  echo "/etc/blackbox/blackbox.conf exists">>/boot/log.txt
-  if [ ! -f "/usr/sbin/blackbox" ]; then
-    #if [ "$DEVMODE" == "1"]; then
-    echo "Creating symbolic link /usr/sbin blackbox">>/boot/log.txt
-    ln -s /boot/installsrc/usr/sbin/blackbox /usr/sbin
-    chmod +x /boot/installsrc/usr/sbin/blackbox
-    #else
-    #   echo "Installing /usr/sbin/blackbox">>/boot/log.txt
-    #   cp -f /boot/installsrc/usr/sbin/blackbox /usr/sbin
-    #   chmod +x /usr/sbin/blackbox
-    fi
-
-  fi
-
-  echo "Running /usr/sbin/blackbox">>/boot/log.txt
-  /usr/sbin/blackbox install
-
-else
-  echo "/etc/blackbox/blackbox.conf doesnot exist ">>/boot/log.txt
-fi
+osbox install
 
 
-echo "Automation_Custom_Script.sh has ended">>/boot/log.txt
+# A function to clone a repo
+#make_repo() {
+#    # Set named variables for better readability
+#    local directory="${1}"
+#    local remoteRepo="${2}"
+#    # The message to display when this function is running
+#    str="Clone ${remoteRepo} into ${directory}"
+#    # Display the message and use the color table to preface the message with an "info" indicator
+#    printf "  %b %s..." "${INFO}" "${str}"
+#    # If the directory exists,
+#    if [[ -d "${directory}" ]]; then
+#        # delete everything in it so git can clone into it
+#        rm -rf "${directory}"
+#    fi
+#    # Clone the repo and return the return code from this command
+#    git clone -q --depth 20 "${remoteRepo}" "${directory}" &> /dev/null || return $?
+#    # Show a colored message showing it's status
+#    printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+#    # Always return 0? Not sure this is correct
+#    return 0
+#}
 
-exit 0
+#if [ ! -d /usr/local/osblackbox ]; then
+#  make_repo /usr/local/osblackbox https://
+#fi
 
-
-
-
-
-
-
-
-
-
-
-#
-#  /etc/blackbox/ dir with options.
-#
-#  /usr/lib/blackbox    Executable
-#  /usr/sbin/blackbox   Executable
-#  /usr/share/blackbox/ Dir with libraries
-#
-
-# Custom Script (post-networking and post-DietPi install)
-# - Allows you to automatically execute a custom script at the end of DietPi install.
-# - Option 0 = Copy your script to /boot/Automation_Custom_Script.sh and it will be executed automatically.
-# - Option 1 = Host your script online, then use e.g. AUTO_SETUP_CUSTOM_SCRIPT_EXEC=https://myweb.com/myscript.sh and it will be downloaded and executed automatically.
-# - Executed script log: /var/tmp/dietpi/logs/dietpi-automation_custom_script.log
-
-
-echo "2" > $BB_STATE
-
-echo "Automation_Custom_Script.sh A ">>/boot/log.txt
-
-
-
-
-
-
-
-echo "Automation_Custom_Script.sh B ">>/boot/log.txt
-
-#devicelog "Automation_Custom_Script.sh start."
-
-
-# FILE=/boot/blackbox/hardware.json
-# check if hardwaredata exists, then register.
-if [ -f "$BB_JSON" ]; then
-    # the file exists!
-    echo "run 2_registerhardware" > $BB_STATE
-    echo "run 2_registerhardwar ">>/boot/log.txt
-    # this means the hardware-detect has already run.
-    # we need to register the hardware in our product db
-    bash /usr/share/blackbox/2_registerhardware.sh
-    # remove the helper files.
-else
-    #telegram "EXISTS: $FILE"
-    echo "BB_JSON doesnt exist? ($BB_JSON) ">>/boot/log.txt
-    echo "no bbjson" > $BB_STATE
-fi
-
-
-if [[ $DEVMODE = 0 ]] ; then
-  devicelog "info,rm -f /usr/share/blackbox/2_registerhardware.sh"
-  rm -f /usr/share/blackbox/2_registerhardware.sh
-fi
-
-
-
-
-BBID=0
-if [ -f "$BB_HASH" ]; then
-   #telegram "EXISTS: $FILE"
-   BBID=$(<$BB_HASH)
-else
-  echo "BB_HASH doesnt exist? ($BB_HASH) ">>/boot/log.txt
-fi
-
-find_IPv4_information
-
-devicelog "info,$BBID,$IPV4_ADDRESS"
-
-#  /var/tmp/dietpi/logs/dietpi-automation_custom_prescript.log
-#  /var/tmp/dietpi/logs/dietpi-automation_custom_script.log
-
-#devicelog "Automation_Custom_Script.sh end."
-
-
-
-exit 0
+#if [ -f "/etc/blackbox/blackbox.conf" ]; then
+#  source "/etc/blackbox/blackbox.conf"
+#  echo "/etc/blackbox/blackbox.conf exists">>/boot/log.txt
+#  if [ ! -f "/usr/sbin/blackbox" ]; then
+#    #if [ "$DEVMODE" == "1"]; then
+#    echo "Creating symbolic link /usr/sbin blackbox">>/boot/log.txt
+#    ln -s /boot/installsrc/usr/sbin/blackbox /usr/sbin
+#    chmod +x /boot/installsrc/usr/sbin/blackbox
+#    fi
+#  fi
+#  echo "Running /usr/sbin/blackbox">>/boot/log.txt
+#  /usr/sbin/blackbox install
+#else
+#  echo "/etc/blackbox/blackbox.conf doesnot exist ">>/boot/log.txt
+#fi
+#echo "Automation_Custom_Script.sh has ended">>/boot/log.txt
+#exit 0
